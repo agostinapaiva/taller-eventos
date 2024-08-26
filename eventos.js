1b63f0e26e78fd948473a3423b7afb935c807629
agostinapaiva <agospg7@gmail.com>
2024-08-26 17:17:08 -0300

    prueba arreglos

--- eventos.js
+++ eventos.js
@@ -1,3 +1,4 @@
-document.addEventListener("click", function alertaDiv(){
+let div=document.getElementById("div")
+document.addEventListener("click", function alertaDiv(div){
 alert("Hola! Soy el div")
 });
